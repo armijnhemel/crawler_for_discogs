f057b779a86fd6b16672ea3c1109c7bd09dbc876
Armijn Hemel <armijn@tjaldur.nl>
2023-10-05 16:12:44 +0200

    init nix configuration

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+  # Use `niv update` to update nixpkgs.
+  # See https://github.com/nmattia/niv/
+  sources = import ./nix/sources.nix;
+
+  #pkgs = import sources.nixpkgs { config.allowUnfree = true; overlays = [ easyocrOverlay ]; };
+  pkgs = import sources.nixpkgs { config.allowUnfree = true; overlays = [ ]; };
+
+  my-python = pkgs.python3.withPackages (p: with p; [
+    click
+    dulwich
+    easyocr
+    defusedxml
+    pyyaml
+    redis
+    requests
+  ]);
+    
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    my-python
+  ];
+}
