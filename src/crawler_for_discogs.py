d6f133e1b0c928d17b41a1cfe9bcd9cc70eeea52
Armijn Hemel <armijn@tjaldur.nl>
2023-10-07 21:07:59 +0200

    add support to remove certain fields from the data

--- src/crawler_for_discogs.py
+++ src/crawler_for_discogs.py
@@ -21,19 +21,41 @@ except ImportError:
     from yaml import Loader
 
 # process json: sort, cleanup
-def process_json(json_data, config):
-    pass
+def process_json(json_data, removes):
+    for r in removes:
+        try:
+            if '/' in r:
+                key1, key2 = r.split('/', 1)
+                del json_data[key1][key2]
+            else:
+                del json_data[r]
+        except KeyError:
+            pass
+    print(json.dumps(json_data, sort_keys=True, indent=4))
 
 @click.command(short_help='Continuously grab data from the Discogs API and store in Git')
 @click.option('--config-file', '-c', required=True, help='configuration file (YAML)', type=click.File('r'))
 @click.option('-v', '--verbose', is_flag=True, help='Enable debug logging')
 def main(config_file, verbose):
     # read the configuration file. This is in YAML format
+    removes = []
     try:
         config = load(config_file, Loader=Loader)
+        if 'fields' in config:
+            if 'remove' in config['fields']:
+                for r in config['fields']['remove']:
+                    removes.append(r)
     except (YAMLError, PermissionError, UnicodeDecodeError) as e:
         print(f"Cannot open configuration file, exiting, {e}", file=sys.stderr)
         sys.exit(1)
+
+    try:
+        with open('/home/armijn/discogs-data/json/24/24639869.json', 'rb') as json_file:
+            json_data = json.load(json_file)
+            process_json(json_data, removes)
+    except Exception as e:
+        print(e)
+        pass
 
 
 if __name__ == "__main__":
